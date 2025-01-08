72fe3e37ef58e2e5d4106fd0db36273ce7493093
jeffpodmayer <jeff.podmayer@gmail.com>
2025-01-08 15:33:27 -0800

    added availability model

--- backend/hut_reminder/models.py
+++ backend/hut_reminder/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Hut - name, location
 class Hut(models.Model):
-  name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
   
-  def __str__(self):
-    return self.name
+    def __str__(self):
+      return self.name
+  
+
+# Availabilty 
+class Availability(models.Model):
+    hut = models.ForeignKey(Hut, on_delete=models.CASCADE, related_name="availabilties")
+    date = models.DateField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+      return f"{self.hut.name} on {self.date} ({'Available' if self.is_available else 'Not Available'})"
+
 
 # Reminder - start_date, end_date, notification type, is_active
 class Reminder(models.Model):
