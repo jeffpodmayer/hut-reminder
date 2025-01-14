5de818b643888c94499b753788e0ce6ea9e68bd9
jeffpodmayer <jeff.podmayer@gmail.com>
2025-01-14 13:56:14 -0800

    connected sqlitedb and checked table creation

--- backend/hut_reminder/app.py
+++ backend/hut_reminder/app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Hut, Availability, Reminder
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hut_reminder.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def home():
+    print(Hut.__tablename__)
+    print(Availability.__tablename__)
+    print(Reminder.__tablename__)
     return "Hello, Flask!"
 
 if __name__ == "__main__":
