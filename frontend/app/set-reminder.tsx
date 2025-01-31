13f424f8dac84fb9bc0585c4a69d180e241a13b5
jeffpodmayer <jeff.podmayer@gmail.com>
2025-01-30 19:53:54 -0800

    frontend work

--- frontend/app/set-reminder.tsx
+++ frontend/app/set-reminder.tsx
@@ -1,9 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const SetReminderScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Set Reminders Screen</Text>
+      <View style={styles.emailContainer}>
+        <Text style={styles.inputLabel}>Enter Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Email"
+          clearButtonMode="while-editing"
+          autoComplete="email"
+          keyboardType="email-address"
+        />
+      </View>
+      <View style={styles.dateContainer}>
+        <Text style={styles.inputLabel}>Dates of Reminder</Text>
+      </View>
     </View>
   );
 };
@@ -13,7 +25,26 @@ export default SetReminderScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  emailContainer: {
     justifyContent: "center",
     alignItems: "center",
+    paddingTop: 50,
+  },
+  inputLabel: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    width: 250,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  dateContainer: {
+    marginTop: 15,
   },
 });
