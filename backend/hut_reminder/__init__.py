b7275f9f8f6285276e5167fb1af83a26ab4c89d2
jeffpodmayer <jeff.podmayer@gmail.com>
2025-02-03 15:53:57 -0800

    factory pattern for init & app

--- backend/hut_reminder/__init__.py
+++ backend/hut_reminder/__init__.py
@@ -2,23 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models import db, Hut, Availability, Reminder
 
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hut_reminder.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hut_reminder.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
-db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
-with app.app_context():
-    db.create_all()
+    @app.route('/')
+    def home():
+        print(Hut.__tablename__)
+        print(Availability.__tablename__)
+        print(Reminder.__tablename__)
+        return "Hello, Flask!"
 
-@app.route('/')
-def home():
-    print(Hut.__tablename__)
-    print(Availability.__tablename__)
-    print(Reminder.__tablename__)
-    return "Hello, Flask!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return app
