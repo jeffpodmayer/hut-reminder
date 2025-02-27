5cf78b56d51b23bb3bce636d4128ece6cd8d731d
jeffpodmayer <jeff.podmayer@gmail.com>
2025-02-26 21:31:52 -0800

    reminders saved to database

--- backend/hut_reminder/app/routes/reminder_routes.py
+++ backend/hut_reminder/app/routes/reminder_routes.py
@@ -1,36 +1,38 @@
 from flask import Blueprint, request, jsonify
 from ..models.reminder import Reminder, db
+from datetime import datetime
 
-# Create a Blueprint for your routes
 reminder_bp = Blueprint('reminder', __name__)
 
 @reminder_bp.route('/createReminder', methods=['POST'])
 def create_reminder():
     data = request.get_json()  # Get the JSON data from the request
-    print("Received data:")
-    print(json.dumps(data, indent=2))
+    print("Received reminder data")
 
-    # Validate the incoming data
+    try: 
+        start_date = datetime.strptime(data['start_date'], '%Y-%m-%d')
+        end_date = datetime.strptime(data['end_date'], '%Y-%m-%d')
+    except ValueError:
+        return jsonify({'error': 'Invalid date format'}), 400
+
     if not data or 'user_email' not in data or 'start_date' not in data or 'end_date' not in data or 'huts' not in data:
         return jsonify({'error': 'Missing required fields'}), 400
 
-    # Create reminders for each hut in the huts array
     reminders = []
     for hut_id in data['huts']:
         new_reminder = Reminder(
             user_email=data['user_email'],
-            start_date=data['start_date'],  # Ensure this is in the correct format (YYYY-MM-DD)
-            end_date=data['end_date'],        # Ensure this is in the correct format (YYYY-MM-DD)
+            start_date=start_date,  
+            end_date=end_date,        
             hut_id=hut_id
         )
         reminders.append(new_reminder)
-        print(new_reminder)
 
     # Add the new reminders to the session and commit
-    # db.session.add_all(reminders)
-    # db.session.commit()
+    db.session.add_all(reminders)
+    db.session.commit()
 
-    return jsonify({'message': 'Reminders created successfully', 'reminder_ids': [reminder.id for reminder in reminders]}), 201
+    return '', 201
 
 # @reminder_bp.route('/getReminders', methods=['GET'])
 # def get_reminders_by_email():
